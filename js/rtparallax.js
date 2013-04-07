21d7e2cb50034815feb46fb3471eebd1f561d102
Joseph Russell <jrussell@redtettemer.com>
2013-04-07 16:41:40 -0400

    Adding internals from previous work

--- js/rtparallax.js
+++ js/rtparallax.js
@@ -9,7 +9,11 @@
         /* ---------- Private internal variables */
 
         // The default options
-        var _defaults = { };
+        var _defaults = {
+			'images': [],
+			'height': 300,
+			'width': '100%'
+		};
 
         // Overwrite the default options with any user-provided options
         var _options = $.extend({}, _defaults, options);
@@ -21,7 +25,43 @@
         /* ---------- Internal methods */
 
         // Set up the initial plugin structure
-        var initialize = function(options) { };
+        var initialize = function() { 
+		
+            // Apply base level default CSS styling to the div
+            _element.css({
+                'height': _options.height,
+                'width': _options.width
+            });
+
+            // Create an HTML structure to be used for the parallax div
+            var newStructure = 
+                $('<div></div>', { 'id': 'rtp-container' })
+                    .css({ 
+                        'height': '100%',
+                        'position': 'relative'
+                    });
+		
+            // Empty out the div and append the structure created above
+            _element.empty().append(newStructure);
+
+            // Add in the images to the container div
+            var rtpcontainer = $('#rtp-container', _element);
+
+            $.each(_options.images, function(index, value) {
+                rtpcontainer
+                    .append(
+                        $('<div></div>', { 'id': 'rtp-bg1' })
+                            .css({ 
+                                'background': "url('" + value + "') top left no-repeat",
+                                'height': rtpcontainer.height() + 'px',
+                                'left': 0,
+                                'position': 'absolute',
+                                'top': 0,
+                                'width': '100%'
+                            })
+                    );
+            });
+		};
 
 
         /* ---------- Publicly-available methods */
