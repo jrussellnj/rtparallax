1c54c503184c2bdd68a426f993724b8b0b03b8fc
Joseph Russell <jrussell@redtettemer.com>
2013-04-16 14:44:04 -0400

    Initial commit

--- js/rtparallax.js
+++ js/rtparallax.js
@@ -1,8 +1,10 @@
+var l = function() { console.log($.makeArray(arguments).join(' ')); };
+
 // RT + Parallax; A lightweight plugin that sets up the bare minimum HTML structure necessary for turning an 
 // empty block-level element into a parallaxing window with public methods for binding to your own HTML elements.
 //
 // Written for Red Tettemer + Partners by Joseph Russell
-; (function($) {
+; (function($, window, undefined) {
 
     $.fn.rtparallax = function(options) {
 
@@ -35,7 +37,9 @@
 				// This gets called after a slide has completed
 				'onSlideComplete': $.noop,
 
-				// How far in pixels to slide each time the next or previous button is hit
+				// How far in pixels to slide each time the next or previous button is hit. This can either be
+                // a single value, for a consistent slide each time, or an array where each value represents
+                // how far to slide after each slide
 				'slideDistance': '',
 
 				// The amount of time the animation will take (in milliseconds)
@@ -94,6 +98,8 @@
 				var rtpcontainer = $('#rtp-container', _element);
 
 				$.each(_options.images, function(index, value) {
+                    var theImage = $('<img></img>', { 'src': value });
+
 					rtpcontainer
 						.append(
 							$('<div></div>', { 'id': 'rtp-bg' + (index + 1) })
@@ -105,22 +111,17 @@
 									'top': 0,
 									'width': '100%'
 								})
-								.append($('<img></img>', { 'src': value }))
+								.append(theImage)
 						);
+
+                        // Record the background image width and figure out the number of "pages" in the slider
+                        if (index == 0) {
+                            theImage.load(function() {
+                                _backgroundImageWidth = $(this).width();
+                                _figurePageCount();
+                            });
+                        }
 				});
-
-				// Figure out the number of "pages" in the slider based either on the distance to slide
-				// that the user provided or, if that wasn't specified, the width of the background image
-				var tempImage = new Image();
-				tempImage.src = _options.images[0];
-				_backgroundImageWidth = tempImage.width;
-
-				if (_options.slideDistance != '') {
-					_this.pages = Math.ceil(_backgroundImageWidth / _options.slideDistance);
-				}
-				else {
-					_this.pages = Math.ceil(_backgroundImageWidth / $('#rtp-container').width());
-				}
 
 				// Bind the next and previous buttons to sliding left and right
 				_options.nextButton.bind('click', function(ev) {
@@ -133,6 +134,11 @@
 					_this.slideLeft();
 				});
 
+                // Recalculate how many pages the slider has as the window is resized
+                $(window).resize(function() {
+                    _figurePageCount();
+                });
+
 				// If the user has provided a function to call after initialization, call it here
 				_options.onLoadComplete.call(_this);
 			};
@@ -143,15 +149,37 @@
 				// If the user has provided a function to call before the slide begins, call it here
 				_options.onSlideBegin.call(_this);
 
-				// Either the pixel width of the element that contains the divs whose images 
-				// we want to slide or the value in pixels that was provided by the user
-				var amountToSlide = _options.slideDistance ? _options.slideDistance : $('#rtp-container').width();
+                // Figure out how much to slide the furthest down background image over
+                // var amountToSlide = _options.slideDistance ? _options.slideDistance : $('#rtp-container').width();
+                var amountToSlide;
+
+                if (_options.slideDistance != '') {
+                    if ($.isArray(_options.slideDistance)) {
+						if (direction == 'right') {
+							amountToSlide = _options.slideDistance[parseInt(_this.currentPage - 1)]
+						}
+						else {
+							amountToSlide = _options.slideDistance[parseInt(_this.currentPage - 2)]
+						}
+                    }
+                    else {
+                        amountToSlide = _options.slideDistance;
+                    }
+                }
+                else {
+                    amountToSlide = $('#rtp-container').width();
+                }
+
+                // How much to step up, as a percentage, the animation speed as we go up the layers of images
+                var stepAmount = .25;
 
 				// Cache the parallax container for use below
 				var rtpcontainer = $('#rtp-container');
 
 				// Slide each image in the parallax containing element to the right, increasing
 				// the speed by double as we get closer to the top layer.
+                var thisStep = 1;
+
 				rtpcontainer.children().each(function(index, element) {
 
 					// Get the image to slide and its current left margin
@@ -164,7 +192,7 @@
 							var newMarginToSlideTo = 0;
 						}
 						else {
-							var newMarginToSlideTo = (currentLeftMargin - parseInt(amountToSlide * (index + 1)));
+							var newMarginToSlideTo = (currentLeftMargin - parseInt(amountToSlide * thisStep));
 
 							// Make sure the right side of the furthest down background image won't slide past the right side of the container
 							if (((Math.abs(newMarginToSlideTo) + rtpcontainer.width()) > _backgroundImageWidth) && (index == 0)) {
@@ -177,7 +205,7 @@
 							var newMarginToSlideTo = (thisImage.width() - (!isNaN(_options.width) ? _options.width : rtpcontainer.width())) * -1;
 						}
 						else {
-							var newMarginToSlideTo = (currentLeftMargin == 0) ? 0 : (currentLeftMargin + parseInt(amountToSlide * (index + 1)));
+							var newMarginToSlideTo = (currentLeftMargin == 0) ? 0 : (currentLeftMargin + parseInt(amountToSlide * thisStep));
 
 							// Make sure the left side of both images are flush up against the left side of the container if we're sliding
 							// an uneven amount of pixels back to the left
@@ -189,6 +217,8 @@
 
 					// Carry out the sliding animation
 					thisImage.stop(true, true).animate( { 'margin-left': newMarginToSlideTo + 'px' }, _options.speed, 'jswing', function() { _options.onSlideComplete.call(_this) });
+
+                    thisStep += stepAmount;
 				});
 
 				// Update what the current page is
@@ -209,6 +239,25 @@
 					}
 				}
 			};
+
+            // Figure out the number of "pages" in the slider based either on the distance to slide
+            // that the user provided or, if that wasn't specified, the width of the background image
+            var _figurePageCount = function() {
+                if (_options.slideDistance != '') {
+                    var widthSoFar = $('#rtp-container').width();
+                    var pagesSoFar = 1;
+
+                    do {
+                        widthSoFar += $.isArray(_options.slideDistance) ? _options.slideDistance[pagesSoFar - 1] : _options.slideDistance;
+                        pagesSoFar++;
+                    } while (widthSoFar < _backgroundImageWidth);
+
+                    _this.pages = pagesSoFar;
+                }
+                else {
+                    _this.pages = Math.ceil(_backgroundImageWidth / $('#rtp-container').width());
+                }
+            };
 
 
 			/* ---------- Publicly-available methods */
@@ -234,4 +283,4 @@
 		/* ---------- Return this object so people can actually call our methods */
 		return new RTParallax(this, options);
     }
-})(jQuery);
+})(jQuery, window, undefined);
